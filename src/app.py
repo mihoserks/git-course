04b72d6b76152a0bf75631b7fde8f6ce3c7e255e
mihoserks <49573514+mihoserks@users.noreply.github.com>
2024-01-17 16:11:48 -0600

    Update app.py

--- src/app.py
+++ src/app.py
@@ -14,3 +14,8 @@ def greating():
 def sum(a: int, b: int):
     nums_sum = a + b
     return  f"La suma es: {str(nums_sum)}"
+
+@app.route('/multiply/<int:a>/<int:b>')
+def multiply (a: int, b: int):
+    result = float (a * b)
+    return f"El resultado de la multiplicación es: {str(result)}"
